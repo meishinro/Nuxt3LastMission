a4af8597f4ef67cdc5cc340f1af03656817b1716
Ming Chen Lu <mingchenlu@MingdeMacBook-Air.local>
2024-12-08 19:28:05 +0900

    導入自動整形tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>this is homepage</h1>
+  <h1 class="mr-3 mt-2 pb-5 text-primary">this is homepage</h1>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   theme: {
     extend: {
       // 在這裡添加您的自定義配置
-        colors: {
-            white: '#ffffff',
-            black: '#000000',
-            primary: '#BF9D7D',
-            
-        },
-    }
-  }
-}
+      colors: {
+        white: "#ffffff",
+        black: "#000000",
+        primary: "#BF9D7D",
+      },
+    },
+  },
+};
