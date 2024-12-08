d071482190a23882aeefb9afbb4e2568485549fd
Ming Chen Lu <mingchenlu@MingdeMacBook-Air.local>
2024-12-08 18:51:12 +0900

    導入eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   typescript: {
-    typeCheck: true
+    typeCheck: true,
   },
-  css: [
-    '@/assets/stylesheets/all.scss', 
-  ],
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
-})
+  css: ["@/assets/stylesheets/all.scss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+});
