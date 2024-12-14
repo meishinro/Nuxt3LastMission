ef76302ddb3312d486fcd72f86f05a1275173916
Ming Chen Lu <mingchenlu@MingdeMacBook-Air.local>
2024-12-14 13:57:22 +0900

    按鈕、layout、header

---
+++ layouts/member.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>會員專區</h1>
+        <div class="bg-gray-700 p-4">
+        <CustomButton variant="primary">按我</CustomButton>
+        </div>
+    </div>
+</template>
---
+++ pages/sample.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-slate-500 pt-[100px]">
+    <h1>Sample Page</h1>
+    <div class="bg-gray-700 p-4">
+      <CustomButton variant="primary">按我</CustomButton>
+    </div>
+  </div>
+</template>
